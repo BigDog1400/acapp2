45c3f00d1bb555d34700aa2b3fed0600e4204240
Isaac Peña <isaac.pr99@gmail.com>
2020-11-29 11:55:36 -0400

    Add BroserRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+
+
+const app=(<BrowserRouter>
+  <App></App>
+</BrowserRouter>)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {app}
+ </React.StrictMode>,
   document.getElementById('root')
 );
 
