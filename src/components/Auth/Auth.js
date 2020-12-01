0b469f97c026e9f06b423b07f79bfca330110d43
Isaac Peña <isaac.pr99@gmail.com>
2020-12-01 03:20:37 -0400

    Agregar Formik a registro de formulario

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,44 +1,83 @@
 import React, { Component } from 'react';
-import {Container, Button, Form, Card, Row} from 'react-bootstrap';
+import {Container, Button, Card, Row, FormGroup, Form, FormControl} from 'react-bootstrap';
+import {Formik, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
 import Logo from '../../assets/logo-black.png';
+const initialValues= {
+  email: '',
+  password: '',
+}
+
+const validationSchema = Yup.object({
+  email: Yup.string().required('Por favor introduzca un email'),
+  password: Yup.string().required('Por favor ingrse una contraseña')
+})
+
 class Auth extends Component {
-    handlerSubmit = event =>{
-        event.preventDefault();
-        console.log(event);
-    }
-    render() {
-        return (
-          <Container className='mx-auto d-flex pt-5' >
-            <Card className="mx-auto">
-            <Card.Img src={Logo}  variant='top'
-            className="mx-auto mt-2"
-            style={{width: '100px'}} />
-              <Card.Body>
-                <Card.Title>Registro</Card.Title>
-                <Form onSubmit={this.handlerSubmit}>
+  handlerSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+  onSubmitHandler = (values) => {
+    console.log(values);
+  };
+  render() {
+    return (
+      <Container className='mx-auto d-flex pt-5'>
+        <Card className='mx-auto'>
+          <Card.Img
+            src={Logo}
+            variant='top'
+            className='mx-auto mt-2'
+            style={{ width: '100px' }}
+          />
+          <Card.Body>
+            <Card.Title>Registro</Card.Title>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={this.onSubmitHandler}
+            >
+              {({handleSubmit, values,handleChange}) => (
+                <Form>
+                {console.log(values)}
                   <Form.Group controlId='email'>
                     <Form.Label>Correo Electronico</Form.Label>
-                    <Form.Control type='email' placeholder='Enter an email' />
+                    <Field
+                      component={FormControl}
+                      type='email'
+                      name='email'
+                      onChange={handleChange}
+
+                      nameplaceholder='Enter an email'
+                      values={values.email}
+                    />
                     <Form.Text variant='light'>
                       Esto te ayudara a mantener tus rutinas registradas
                     </Form.Text>
                   </Form.Group>
                   <Form.Group controlId='password'>
                     <Form.Label>Contraseña</Form.Label>
-                    <Form.Control
+                    <Field
+                      component={FormControl}
                       type='password'
-                      placeholder='Enter an email'
+                      name='password'
+                      placeholder='Password'
+                      values={values.password}
+                      onChange={handleChange}
                     />
                   </Form.Group>
-                  <Button variant='primary' type='submit'>
+                  <Button onClick={handleSubmit} variant='primary'>
                     Submit
                   </Button>
                 </Form>
-              </Card.Body>
-            </Card>
-          </Container>
-        );
-    }
+              )}
+            </Formik>
+          </Card.Body>
+        </Card>
+      </Container>
+    );
+  }
 }
 
 export default Auth;
