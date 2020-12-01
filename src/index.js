2b6d0ee4ee17c7919cf1239ff75f0f965f03bb12
Isaac Peña <isaac.pr99@gmail.com>
2020-12-01 04:46:46 -0400

    Agregar sass

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+import './index.css';
 
 const app=(<BrowserRouter>
   <App></App>
