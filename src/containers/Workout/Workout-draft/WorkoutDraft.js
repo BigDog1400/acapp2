60a66005558b5f9a6381c098b232654d6efdb4bd
Isaac Peña <isaac.pr99@gmail.com>
2020-12-13 16:04:45 -0400

    feat: se agrega redireccionamiento a path /workout

--- src/containers/Workout/Workout-draft/WorkoutDraft.js
+++ src/containers/Workout/Workout-draft/WorkoutDraft.js
@@ -8,8 +8,9 @@ import {connect} from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBolt } from '@fortawesome/free-solid-svg-icons'
 import {setWorkout} from '../../../store/actions/index';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 const WorkoutDraft = (props) => {
-    const {setWorkout} = {...props}
+    const {setWorkout,draftDone} = {...props}
     const [listExercises,setListExercises] = useState({})
     const [exercisesOrder,setExercisesOrder] = useState([])
     const addExercise = values =>{
@@ -32,24 +33,43 @@ const WorkoutDraft = (props) => {
         return prevState;
       });
     }
+    const redirectWorkoutView = draftDone ? <Redirect to='/workout/routine'/> : null;
     return (
-      <div className='container_form-workout'>
-        <div className='form-workout'>
-          <Input_Exercise newExercise={addExercise}></Input_Exercise>
+      <React.Fragment>
+        {redirectWorkoutView}
+        <div className='container_form-workout'>
+          <div className='form-workout'>
+            <Input_Exercise newExercise={addExercise}></Input_Exercise>
+          </div>
+          <div className='workout-list'>
+            {exercisesOrder.map((exerciseID) => (
+              <List_item_exercise
+                key={exerciseID}
+                {...listExercises[exerciseID]}
+                exerciseID={exerciseID}
+                handlerRemove={removeExercise}
+              />
+            ))}
+          </div>
+          {exercisesOrder.length > 0 ? (
+            <Button
+              className='button-start'
+              onClick={setWorkoutHandler}
+              variant='dark'
+              size='lg'
+            >
+              EMPEZAR{' '}
+              <FontAwesomeIcon style={{ color: 'yellow' }} icon={faBolt} />
+            </Button>
+          ) : null}
         </div>
-        <div className='workout-list'>
-          {exercisesOrder.map((exerciseID) => (
-            <List_item_exercise
-              key={exerciseID}
-              {...listExercises[exerciseID]}
-              exerciseID={exerciseID}
-              handlerRemove={removeExercise}
-            />
-          ))}
-        </div>
-        {exercisesOrder.length > 0 ? <Button className='button-start' onClick={setWorkoutHandler} variant="dark"  size="lg">EMPEZAR <FontAwesomeIcon style={{color:'yellow'}} icon={faBolt}/></Button> : null}
-      </div>
+      </React.Fragment>
     );
 }
 
-export default connect(null, {setWorkout})(WorkoutDraft);
+const mapStateToProps = ({workout}) => {
+  return {
+    draftDone : workout.completed
+  }
+}
+export default connect(mapStateToProps, {setWorkout})(WorkoutDraft);
--- src/containers/Workout/Workout-routine/WorkoutRoutine.js
+++ src/containers/Workout/Workout-routine/WorkoutRoutine.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
-const WorkoutRoutine = () => {
-    return (
-        <div>
-            <h1>I will train now!</h1>
-        </div>
-    );
+import {connect} from 'react-redux';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+const WorkoutRoutine = (props) => {
+  const { draftDone } = { ...props };
+  const redirectWorkoutView = !draftDone ? (
+    <Redirect to='/workout/draft' />
+  ) : null;
+  return (
+    <React.Fragment>
+    {redirectWorkoutView}
+      <div>
+        <h1>I will train now!</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+const mapStateToProps = ({workout}) =>{
+    return { draftDone: workout.completed };
 }
-
-export default WorkoutRoutine;
+export default connect(mapStateToProps)(WorkoutRoutine);
